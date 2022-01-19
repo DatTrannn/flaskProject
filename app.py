ff0e76f5c51c616a2fdc1e662ff63c4e0f6f58cf
Dattran <dat0609.tran@gmail.com>
2022-01-19 21:07:16 +0700

    Add greet function and route

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return '<h1>Hello World :)</h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/dat')
+def greet():
+    return 'Hello {}'.format("Dat")
 
 
 if __name__ == '__main__':
